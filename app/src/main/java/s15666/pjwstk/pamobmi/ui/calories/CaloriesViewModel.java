43c5e1d8b3e8963b037ce179ef42027432c6a9a3
Tomasz Szczodrowski <bdn583@gmail.com>
2020-03-17 13:22:48 +0100

    Add CaloriesViewModel

---
+++ app/src/main/java/s15666/pjwstk/pamobmi/ui/calories/CaloriesViewModel.java
@@ -0,0 +1,46 @@
+package s15666.pjwstk.pamobmi.ui.calories;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+class CaloriesViewModel extends ViewModel {
+
+    private final MutableLiveData<Gender> gender = new MutableLiveData<>();
+    private final MutableLiveData<Integer> age = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> isValid = new MutableLiveData<>();
+
+    CaloriesViewModel() {
+        gender.setValue(Gender.MALE);
+        isValid.setValue(Boolean.FALSE);
+    }
+
+    MutableLiveData<Gender> getGender() {
+        return gender;
+    }
+
+    void setGender(Gender gender) {
+        this.gender.setValue(gender);
+        checkValid();
+    }
+
+    MutableLiveData<Integer> getAge() {
+        return age;
+    }
+
+    void setAge(Integer age) {
+        this.age.setValue(age);
+        checkValid();
+    }
+
+    private void checkValid() {
+        Boolean newValue = age.getValue() != null && age.getValue() > 0;
+        if(isValid.getValue() != newValue) {
+            isValid.setValue(newValue);
+        }
+    }
+
+    MutableLiveData<Boolean> isValid() {
+        return isValid;
+    }
+
+}
