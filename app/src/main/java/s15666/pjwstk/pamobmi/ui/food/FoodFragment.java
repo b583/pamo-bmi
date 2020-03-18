a26973c0ee7e4d5f2f52913a6771cc0ffcec6a2e
Tomasz Szczodrowski <bdn583@gmail.com>
2020-03-18 11:38:52 +0100

    Get BmiViewModel in FoodFragment

--- app/src/main/java/s15666/pjwstk/pamobmi/ui/food/FoodFragment.java
+++ app/src/main/java/s15666/pjwstk/pamobmi/ui/food/FoodFragment.java
@@ -7,12 +7,23 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import s15666.pjwstk.pamobmi.R;
+import s15666.pjwstk.pamobmi.bmi.BmiViewModel;
 
 public class FoodFragment extends Fragment {
 
+    private BmiViewModel model;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_food, container, false);
+        View view = inflater.inflate(R.layout.fragment_food, container, false);
+        model = new ViewModelProvider(requireActivity()).get(BmiViewModel.class);
+
+        init();
+        return view;
+    }
+
+    private void init() {
     }
 }
