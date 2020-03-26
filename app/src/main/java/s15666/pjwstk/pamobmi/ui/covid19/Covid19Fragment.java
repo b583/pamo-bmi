39ec99cd94e228d3bc090d1f3df46e1589379cfc
Tomasz Szczodrowski <bdn583@gmail.com>
2020-03-26 14:55:09 +0100

    Embed browser into COVID-19 Fragment

--- app/src/main/java/s15666/pjwstk/pamobmi/ui/covid19/Covid19Fragment.java
+++ app/src/main/java/s15666/pjwstk/pamobmi/ui/covid19/Covid19Fragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,8 +13,18 @@ import s15666.pjwstk.pamobmi.R;
 
 public class Covid19Fragment extends Fragment {
 
+    private WebView web;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_covid19, container, false);
+        View view = inflater.inflate(R.layout.fragment_covid19, container, false);
+        web = view.findViewById(R.id.covid19_webview);
+
+        init();
+        return view;
+    }
+
+    private void init() {
+        web.loadUrl("file:///android_asset/covid19_graph.html");
     }
 
 }
