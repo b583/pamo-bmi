a805018efbad32967c0e88e08ef3df040a12eb64
Tomasz Szczodrowski <bdn583@gmail.com>
2020-03-26 16:16:37 +0100

    Show Line Chart for COVID-19 cases in Poland

--- app/src/main/java/s15666/pjwstk/pamobmi/ui/covid19/Covid19Fragment.java
+++ app/src/main/java/s15666/pjwstk/pamobmi/ui/covid19/Covid19Fragment.java
@@ -1,9 +1,13 @@
 package s15666.pjwstk.pamobmi.ui.covid19;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 import androidx.annotation.NonNull;
@@ -23,7 +27,17 @@ public class Covid19Fragment extends Fragment {
         return view;
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     private void init() {
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("PAMO BMI", consoleMessage.sourceId() + ":" + consoleMessage.lineNumber() + " - " + consoleMessage.message());
+                return super.onConsoleMessage(consoleMessage);
+            }
+        });
+
         web.loadUrl("file:///android_asset/covid19_graph.html");
     }
 
